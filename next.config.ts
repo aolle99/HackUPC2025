f72fe6ba0ed3ba26dee94fdd6ad433a3bd449c1a
Àlex Ollé <aolle99@gmail.com>
2025-05-03 16:34:06 +0200

    Refactor ImageUploader and fix parameter reference in API route.
    Replaced default HTML `img` with Next.js `Image` for better optimization in `ImageUploader`. Additionally, corrected the parameter destructuring in the GET API route to resolve an issue with accessing image IDs.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ['localhost'],
+        domains: ['localhost', 'hack-upc-2025.vercel.app'],
     },
 
 };
