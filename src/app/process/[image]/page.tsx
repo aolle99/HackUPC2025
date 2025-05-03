46ccc60a6eb5020f63b88247147f556a33bfd5a2
Àlex Ollé <aolle99@gmail.com>
2025-05-03 22:23:19 +0200

    Update fallback image for missing product content
    Replaced the placeholder image URL with a locally hosted fallback (`noImage.png`) to ensure better consistency and control over missing content visuals. Added the `noImage.png` file to the `public/images` directory.

--- src/app/process/[image]/page.tsx
+++ src/app/process/[image]/page.tsx
@@ -62,8 +62,10 @@ export default async function Page(props: { params: Promise<{ image: string }>})
 
         const fetchHtml = fetch(`${baseURL}?bm-verify=${bmVerifyToken}`);
         const html = await ((await fetchHtml).text());
+        console.error(html)
         // Find the product image
         const imageSrc = html.match( /https:\/\/static\.zara\.net\/assets\/public[^\?]*\.jpg/g)?.shift() || `${siteUrl}/images/noImage.png`;
+        console.error(html.match( /https:\/\/static\.zara\.net\/assets\/public[^\?]*\.jpg/g))
         similarProducts.push({
             ...rawSimilarProducts[similarProducts.length],
             imageSrc,
